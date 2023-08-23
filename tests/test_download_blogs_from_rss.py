4263d6464abb3440445cc227c8742f476a1cd567
eliaselhaddad <eliaselhaddad@icloud.com>
2023-08-23 13:24:41 +0200

    test - makefile

--- tests/test_download_blogs_from_rss.py
+++ tests/test_download_blogs_from_rss.py
@@ -2,4 +2,24 @@ from newsfeed import download_blogs_from_rss
 
 
 def test_import_project() -> None:
-    download_blogs_from_rss.main(blog_name="MIT")
+    print("Running test_import_project...")
+    download_blogs_from_rss.main(blog_name="mit")  # changed from MIT to mit
+    print("Test completed.")
+
+
+def test_imports():
+    essential_modules = [
+        "datetime",
+        "pydantic",
+        "argparse",
+        "pathlib",
+        "requests",
+        "sys",
+        "newsfeed.download_blogs_from_rss",
+    ]
+
+    for module in essential_modules:
+        try:
+            __import__(module)
+        except ImportError:
+            assert False, f"Failed to import {module}"
