9deb97d11a38db98057e91c773c71ef4f9522534
CyrilleKolle <ngidekollecyrille@gmail.com>
2023-08-29 14:28:54 +0200

    Added a check for python major version

--- tests/test_python_version_is_10.py
+++ tests/test_python_version_is_10.py
@@ -6,6 +6,7 @@ def test_python_version():
     # version_info returns a list and from this list i chose the second item which is the python minor version
     # Then I use the assert function to compare both values, and throw an assert error if both values are not 10
     minor_version = sys.version_info[1]
+    major_version = sys.version_info[0]
     assert (
-        minor_version == 10
-    ), f"The expected and required Python version for this project is 3.10.*, but got 3.{minor_version}.*"
+        major_version == 3 and minor_version == 10
+    ), f"The expected and required Python version for this project is 3.10.*, but got {major_version}.{minor_version}.*"
