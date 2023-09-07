870072c5dc2c578e8b015bfba32deb6dbb5aebe5
CyrilleKolle <ngidekollecyrille@gmail.com>
2023-09-08 00:54:25 +0200

    Added a search functionality through titles

--- src/newsfeed/layout.py
+++ src/newsfeed/layout.py
@@ -41,20 +41,18 @@ def create_data_type_dropdown():
         dbc.Card(
             html.Div(
                 [
-                    dbc.CardGroup(
-                        dcc.Dropdown(
-                            id="data-type-dropdown",
-                            className="hidden",
-                            options=[
-                                {"label": "All Blogs", "value": "all_blogs"},
-                                {"label": "Google AI", "value": "google_ai"},
-                                {"label": "MIT", "value": "mit"},
-                                {"label": "Artificial Intelligence Blog", "value": "ai_blog"},
-                                {"label": "OpenAI", "value": "open_ai"},
-                            ],
-                            value="all_blogs",
-                            style={"width": "0px", "height": "0px"},
-                        )
+                    dcc.Dropdown(
+                        id="data-type-dropdown",
+                        className="hidden",
+                        options=[
+                            {"label": "All Blogs", "value": "all_blogs"},
+                            {"label": "Google AI", "value": "google_ai"},
+                            {"label": "MIT", "value": "mit"},
+                            {"label": "Artificial Intelligence Blog", "value": "ai_blog"},
+                            {"label": "OpenAI", "value": "open_ai"},
+                        ],
+                        value="all_blogs",
+                        style={"width": "0px", "height": "0px"},
                     )
                 ],
                 style={"justify-content": "center"},
@@ -70,46 +68,35 @@ def create_blog_choice_dropdown():
     return dbc.Col(
         dbc.Card(
             html.Div(
-                [
-                    dbc.CardGroup(
-                        dbc.Card(
-                            dbc.CardBody(
-                                [
-                                    dbc.Row(
-                                        [
-                                            dbc.Col(
-                                                dcc.Dropdown(
-                                                    id="dropdown-choice",
-                                                    options=[
-                                                        {
-                                                            "label": "All Blogs",
-                                                            "value": "all_blogs",
-                                                        },
-                                                        {
-                                                            "label": "Google AI",
-                                                            "value": "google_ai",
-                                                        },
-                                                        {"label": "MIT", "value": "mit"},
-                                                        {
-                                                            "label": "Artificial Intelligence Blog",
-                                                            "value": "ai_blog",
-                                                        },
-                                                        {"label": "OpenAI", "value": "open_ai"},
-                                                    ],
-                                                    value="all_blogs",
-                                                    style={"width": "300px", "margintop": "100px"},
-                                                ),
-                                                width={"size": 3},
-                                            ),
-                                        ],
-                                        style={"margintop": "25px"},
-                                    )
-                                ]
-                            )
-                        )
-                    )
-                ],
-                style={"display": "flex", "justify-content": "center"},
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id="dropdown-choice",
+                                options=[
+                                    {
+                                        "label": "All Blogs",
+                                        "value": "all_blogs",
+                                    },
+                                    {
+                                        "label": "Google AI",
+                                        "value": "google_ai",
+                                    },
+                                    {"label": "MIT", "value": "mit"},
+                                    {
+                                        "label": "Artificial Intelligence Blog",
+                                        "value": "ai_blog",
+                                    },
+                                    {"label": "OpenAI", "value": "open_ai"},
+                                ],
+                                value="all_blogs",
+                                style={"width": "300px", "margintop": "100px"},
+                            ),
+                        ),
+                    ],
+                    style={"margintop": "25px"},
+                ),
+                style={},
             )
         )
     )
@@ -146,65 +133,159 @@ def language_choice():
     )
 
 
+def logo_row():
+    return html.Div(
+        style={
+            "display": "flex",
+            "justifyContent": "space-between",
+        },
+        children=[
+            html.Div(create_logo(), style={"flex": "1"}),
+            html.Div(create_blog_heading(), style={"flex": "4"}),
+            html.Div(language_choice(), style={"flex": "1"}),
+        ],
+    )
+
+
+def dropdown_select_blogs():
+    return html.Div(
+        dbc.Card(
+            dbc.CardBody(
+                children=[
+                    dbc.Col(
+                        create_data_type_dropdown(),
+                        width={"size": 6, "offset": 3},
+                    ),
+                    dbc.Col(
+                        create_blog_choice_dropdown(),
+                        width={"size": 6, "offset": 3},
+                    ),
+                ]
+            ),
+        ),
+        style={
+            "flex": "3",
+            "display": "flex",
+            "justifyContent": "center",
+            "alignItems": "center",
+            "marginTop": "0px",
+        },
+    )
+
+
+def search_input():
+    return html.Div(
+        dcc.Input(
+            autoComplete="off",
+            placeholder="search in titles",
+            id="search-input",
+            type="text",
+            value="",
+            style={
+                "width": "100%",
+                "height": "100%",
+                "padding": "6px",
+                "fontSize": "16px",
+            },
+        ),
+        style={
+            "flex": "3",
+            "display": "flex",
+            "justifyContent": "center",
+            "alignItems": "center",
+        },
+    )
+
+
+def search_button():
+    return html.Div(
+        html.Button(
+            "search",
+            id="search-btn",
+            style={
+                "height": "100%",
+                "padding": "7px",
+                "fontSize": "14px",
+            },
+        ),
+        style={
+            "flex": "1",
+            "display": "flex",
+            "justifyContent": "left",
+            "alignItems": "left",
+            "width": "20px",
+            "marginLeft": "15px",
+            "height": "100%",
+        },
+    )
+
+
+def header_content():
+    return dbc.Card(
+        dbc.CardBody(
+            dbc.Row(
+                className="mb-4",
+                children=[
+                    dbc.Row(children=[logo_row()]),
+                    dbc.Row(
+                        children=[
+                            html.Div(
+                                children=[
+                                    dropdown_select_blogs(),
+                                    search_input(),
+                                    search_button(),
+                                ],
+                                style={
+                                    "display": "flex",
+                                    "justifyContent": "space-between",
+                                    "maxWidth": "1080px",
+                                    "alignContent": "center",
+                                    "alignItems": "center",
+                                    "margin": "auto",
+                                },
+                            )
+                        ]
+                    ),
+                ],
+            )
+        ),
+        style={
+            "position": "fixed",
+            "top": 0,
+            "width": "100%",
+            "z-index": 1000,
+            "height": "170px",
+            "backgroundColor": "white",
+        },
+    )
+
+
+def news_content():
+    return dbc.Card(
+        dbc.CardBody(
+            dbc.Row(
+                id="content-container",
+                style={
+                    "justifyContent": "center",
+                    "alignItems": "center",
+                    "height": "100vh",
+                    "maxWidth": "1080px",
+                    "alignSelf": "center",
+                    "margin": "0 auto",
+                },
+            )
+        ),
+        style={"marginTop": "170px"},
+    )
+
+
 # creates layout
 def create_layout():
     """Creates the layout for the dashboard"""
     return dbc.Container(
         [
-            dbc.Card(
-                dbc.CardBody(
-                    dbc.Row(
-                        className="mb-4",
-                        children=[
-                            dbc.Row(
-                                children=[
-                                    html.Div(
-                                        style={
-                                            "display": "flex",
-                                            "justifyContent": "space-between",
-                                        },
-                                        children=[
-                                            html.Div(create_logo(), style={"flex": "1"}),
-                                            html.Div(create_blog_heading(), style={"flex": "4"}),
-                                            html.Div(language_choice(), style={"flex": "1"}),
-                                        ],
-                                    )
-                                ]
-                            ),
-                            dbc.Row(
-                                children=[
-                                    dbc.Col(create_data_type_dropdown(), width=1),
-                                    dbc.Col(create_blog_choice_dropdown(), width=7),
-                                ]
-                            ),
-                        ],
-                    )
-                ),
-                style={
-                    "position": "fixed",
-                    "top": 0,
-                    "width": "100%",
-                    "z-index": 1000,
-                    "height": "170px",
-                    "backgroundColor": "white",
-                },
-            ),
-            dbc.Card(
-                dbc.CardBody(
-                    dbc.Row(
-                        id="content-container",
-                        style={
-                            "justifyContent": "center",
-                            "alignItems": "center",
-                            "height": "100vh",
-                            "maxWidth": "1080px",
-                            "alignSelf": "center",
-                            "margin": "0 auto",
-                        },
-                    )
-                ),
-                style={"marginTop": "170px"},
-            ),
+            header_content(),
+            news_content(),
             dcc.Store(id="blogs-df"),
             dcc.Store(id="language-store", data={"language": "english"}),
         ]
