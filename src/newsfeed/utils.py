28cba91c3e3209a5179ffbb5afd68867bac2cbed
eliaselhaddad <eliaselhaddad@icloud.com>
2023-09-11 11:35:00 +0200

    Added Swedish summarization for Discord.

--- src/newsfeed/utils.py
+++ src/newsfeed/utils.py
@@ -7,4 +7,11 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description="Enables running scripts in the terminal")
     parser.add_argument("--blog_name", type=str, help="Name of the specific blog source")
+    parser.add_argument(
+        "--language",
+        type=str,
+        default="en",
+        choices=["en", "sv"],
+        help="Language of summaries: 'en' for English, 'sv' for Swedish",
+    )
     return parser.parse_args()
